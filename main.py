97a945d422541d679af507e56eaf3b4cbaec959d
Egor <egor.lihverov@gmail.com>
2025-09-30 23:23:56 +0300

    Repo init | main.py for 2 numbers

---
+++ main.py
@@ -0,0 +1,11 @@
+def sum_two_numbers(a, b):
+    return a + b
+
+
+def main():
+    result = sum_two_numbers(5, 10)
+    print(f"Сумма двух чисел: {result}")
+
+
+if __name__ == "__main__":
+    main()
